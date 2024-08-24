749fa3096b6e2d5454df24964f1b8ba3b9d321fb
srinu <sopparisrinu637@gmail.com>
2024-08-24 23:44:39 +0530

    add

--- Add.js
+++ Add.js
@@ -5,17 +5,3 @@ const num2 = 3;
 const sum = num1 + num2;
 
 
-
-console.log('The sum of jjjuu' + num1 + ' and ' + num2 + ' is: ' + sum);
-
-
-
-const sub = num1 - num2;
-
-
-console.log('The sub of jjjuu' + num1 + ' and ' + num2 + ' is: ' + sub);
-
-
-const  mul = num1 * num2
-
-console.log("mul", mul);
