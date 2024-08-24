2df49c5840138e2e7615686539f7fb3a84bff31b
ramesh <rameshsoppari8@gmail.com>
2024-08-24 23:07:18 +0530

    sub

--- Add.js
+++ Add.js
@@ -7,15 +7,3 @@ const sum = num1 + num2;
 
 
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
-
-
-const num3 = 7;
-const num4 = 15;
-
-
-const difference = num3 - num4;
-
-console.log('The difference between ' + num3 + ' and ' + num4 + ' is: ' + difference);
-
-
-
