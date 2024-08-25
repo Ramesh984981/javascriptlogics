8dbd7bbda17ba209d7ba307bc9fa4d8c2c2acd1c
srinu <sopparisrinu637@gmail.com>
2024-08-25 11:08:44 +0530

    added largest

---
+++ heighestNumber.js
@@ -0,0 +1,11 @@
+var n1 = 10;
+var n2 = 20;
+var n3 = 30;
+
+if(n1 > n2 && n1 > n3){
+    console.log("heighest is", n1)
+}else if(n2 > n1 && n2 > n3){
+    console.log("heighest is", n2)
+}else{
+    console.log("heighest is", n3)
+}
