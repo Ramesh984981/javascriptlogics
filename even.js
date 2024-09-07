b86d16fb8a45b19c7f89c47ccaa20766e8a4e378
ramesh <rameshsoppari8@gmail.com>
2024-09-07 23:23:18 +0530

    adds

--- even.js
+++ even.js
@@ -1,16 +1,33 @@
-let n = [1,2,3,4]
+// let n = [1,2,3,4]
+
+// function isEven(num){
+//     for(var i=0; i<= num.length; i++){
+//         if(num[i] % 2==0){
+//             console.log(num[i] + " is even");
+//         }else{
+//             console.log(num[i] + " is odd");
+//         }
+//     }
+
+// } 
+
+// const evenNumber = isEven(n)
+
+// console.log("evennumber", evenNumber);
+
+
+
+
+let n1 = [1,2,3,4,5,6,7,8,9,10];
 
 function isEven(num){
-    for(var i=0; i<= num.length; i++){
-        if(num[i] % 2==0){
-            console.log(num[i] + " is even");
-        }else{
-            console.log(num[i] + " is odd");
-        }
+  for (var i=0;i<num.length;i++){
+    if(num[i]%2==0){
+      console.log(num[i] +" is even");
+    }else{
+      console.log(num[i]+" is odd"); 
     }
+  }
+}
 
-} 
-
-const evenNumber = isEven(n)
-
-console.log("evennumber", evenNumber);
+isEven(n1); 
