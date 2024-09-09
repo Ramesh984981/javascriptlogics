a6435a9a2754252d16f1bd3401626544a8db5ed1
ramesh <rameshsoppari8@gmail.com>
2024-09-09 09:06:05 +0530

    obj

--- for.js
+++ for.js
@@ -1,13 +1,22 @@
-for (let i = 0; i < 3; i++) {
-    console.log("Hello, world!");
-}
+// for (let i = 0; i < 3; i++) {
+//     console.log("Hello, world!");
+// }
 
 
 
-for (var i=0;i<5;i++){
-    console.log("Ramesh");
-}
+// for (var i=0;i<5;i++){
+//     console.log("Ramesh");
+// }
 
+
+
+for (var i = 0; i < 10; i++) {
+    if (i == 5) {
+      console.log("Suresh");
+    } else {
+      console.log("Ramesh");
+    }
+  }
 
 
 
---
+++ jsobjects.js
@@ -0,0 +1,13 @@
+
+const student = {
+    firstName: "Jack",
+    rollNo: 32
+  };
+  
+  const student2 = {
+    firstName: "ramesh",
+    rollNo: 43
+  };
+  
+  console.log(student, student2);
+
